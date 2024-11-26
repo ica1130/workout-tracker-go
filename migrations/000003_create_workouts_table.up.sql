a6d1a06caffe3da30294b393182a1187458ddc9f
ica1130 <ilijakrilovic@gmail.com>
2024-11-26 13:30:24 +0100

    feat: created workouts migration files.

---
+++ migrations/000003_create_workouts_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS workouts (
+    id bigserial PRIMARY KEY,
+    member_id bigint NOT NULL REFERENCES members(id) ON DELETE CASCADE,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    duration INTERVAL
+);
