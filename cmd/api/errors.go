3abd1273865ae97bfed7a70d90f617f69d669914
ica1130 <ilijakrilovic@gmail.com>
2024-12-01 12:26:27 +0100

    feat: createad logError helper for logging errors.

---
+++ cmd/api/errors.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
