2477ddbf125a658ba52700aa21be7a74e13693a0
ica1130 <ilijakrilovic@gmail.com>
2024-12-01 12:54:05 +0100

    refactor: updated workouts.go with new error responses.

--- cmd/api/workouts.go
+++ cmd/api/workouts.go
@@ -20,7 +20,7 @@ func (app *application) createWorkoutHandler(w http.ResponseWriter, r *http.Requ
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -32,37 +32,32 @@ func (app *application) createWorkoutHandler(w http.ResponseWriter, r *http.Requ
 
 	err = app.models.Workouts.Insert(workout)
 	if err != nil {
-		http.Error(w, "there was an error while creating an exercise", http.StatusInternalServerError)
-		app.logger.Printf("error: %v", err)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
 	err = app.writeJSON(w, http.StatusCreated, envelope{"workout": workout}, nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		app.logger.Printf("error: %v", err)
-		return
+		app.serverErrorResponse(w, r, err)
 	}
 }
 
 func (app *application) getAllWorkoutsByMemberIDHandler(w http.ResponseWriter, r *http.Request) {
 	memberID, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
 	workouts, err := app.models.Workouts.GetByMemberID(memberID)
 	if err != nil {
-		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
-		app.logger.Printf("error: %v", err)
+		app.serverErrorResponse(w, r, err)
 		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"workouts": workouts}, nil)
 	if err != nil {
-		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
-		app.logger.Printf("error: %v", err)
+		app.serverErrorResponse(w, r, err)
 	}
 }
 
@@ -72,7 +67,7 @@ func (app *application) deleteWorkoutHandler(w http.ResponseWriter, r *http.Requ
 
 	idInt, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
-		http.Error(w, "invalid workout id parameter", http.StatusBadRequest)
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -80,18 +75,15 @@ func (app *application) deleteWorkoutHandler(w http.ResponseWriter, r *http.Requ
 	if err != nil {
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
-			http.NotFound(w, r)
+			app.notFoundResponse(w, r)
 		default:
-			http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
-			app.logger.Printf("error: %v", err)
+			app.serverErrorResponse(w, r, err)
 		}
 		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"message": "workout sucessfully deleted"}, nil)
 	if err != nil {
-		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
-		app.logger.Printf("error: %v", err)
-		return
+		app.serverErrorResponse(w, r, err)
 	}
 }
