db78c5c1323b8b986c432b6cfc262a1947f6d180
ica1130 <ilijakrilovic@gmail.com>
2024-12-02 15:09:14 +0100

    feat: created function for generating Tokens.

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,6 +1,11 @@
 package data
 
-import "time"
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
 
 type Token struct {
 	Plaintext string
@@ -8,4 +13,26 @@ type Token struct {
 	MemberID  int64
 	Expiry    time.Time
 	Scope     string
+}
+
+func generateToken(memberID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		MemberID: memberID,
+		Expiry:   time.Now().Add(ttl),
+		Scope:    scope,
+	}
+
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+
+	return token, nil
 }
