23b7bc8287fb0cde77dd99c4fd0bd3c2caf567fa
ica1130 <ilijakrilovic@gmail.com>
2024-12-02 15:14:18 +0100

    feat: created logic for inserting generated tokens in db.

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/base32"
 	"time"
 )
@@ -35,4 +37,33 @@ func generateToken(memberID int64, ttl time.Duration, scope string) (*Token, err
 	token.Hash = hash[:]
 
 	return token, nil
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) New(memberID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(memberID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Insert(token)
+	return token, err
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	query := `
+		INSERT INTO tokens (hash, member_id, expiry, scope)
+		VALUES ($1, $2, $3, $4)
+	`
+
+	args := []interface{}{token.Hash, token.MemberID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args...)
+	return err
 }
