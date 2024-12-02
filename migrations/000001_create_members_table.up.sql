4e5cedcf1026b33751c4c0f20af4cdc0c0b11fce
ica1130 <ilijakrilovic@gmail.com>
2024-12-02 12:42:36 +0100

    feat: members sql tables have now password hash and activated bool.

--- migrations/000001_create_members_table.up.sql
+++ migrations/000001_create_members_table.up.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS members (
     id bigserial PRIMARY KEY,
     email text UNIQUE NOT NULL,
     name text NOT NULL,
+    password_hash bytea NOT NULL,
+    activated bool NOT NULL,
     height int,
     weight int,
     created_at timestamp(0) with time zone DEFAULT NOW(),
