0c4f9b428c1efa891f6f34f1b515ae3646ad0663
ica1130 <ilijakrilovic@gmail.com>
2024-12-02 12:56:37 +0100

    feat: updated Member struct to have password hash and activated fields.

--- internal/data/members.go
+++ internal/data/members.go
@@ -13,6 +13,8 @@ type Member struct {
 	ID        int64     `json:"id"`
 	Email     string    `json:"email"`
 	Name      string    `json:"name"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
 	Height    int64     `json:"height"`
 	Weight    int64     `json:"weight"`
 	CreatedAt time.Time `json:"created_at"`
