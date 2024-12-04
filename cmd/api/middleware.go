c4424f2900bc7e821e3e2cc95c3c4922683960a3
ica1130 <ilijakrilovic@gmail.com>
2024-12-04 16:23:46 +0100

    feat: created logic for authentication middleware.

---
+++ cmd/api/middleware.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"workout-tracker-go.ilijakrilovic.com/internal/data"
+)
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+
+		authorizationHeader := r.Header.Get("Authorization")
+
+		if authorizationHeader == "" {
+			r = app.contextSetMember(r, data.AnonymousUser)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.invalidAuthenticationTokenResponse(w, r)
+			return
+		}
+
+		token := headerParts[1]
+
+		member, err := app.models.Members.GetForToken("authentication", token)
+		if err != nil {
+			switch {
+			case errors.Is(err, data.ErrRecordNotFound):
+				app.invalidAuthenticationTokenResponse(w, r)
+			default:
+				app.serverErrorResponse(w, r, err)
+			}
+			return
+		}
+
+		r = app.contextSetMember(r, member)
+
+		next.ServeHTTP(w, r)
+	})
+}
